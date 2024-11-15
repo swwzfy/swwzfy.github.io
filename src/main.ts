c97401fa039f9edea8d71bc4f5b5e40a1d473cf0
shaowenwu <swwzfy@163.com>
2024-11-15 13:45:45 +0800

    添加icon

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+
 app.use(ElementPlus)
 app.use(store).use(router).mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <el-button>Default</el-button>
+    <el-icon :size="20">
+      <Edit />
+    </el-icon>
     <h1>This is an about page</h1>
   </div>
 </template>
